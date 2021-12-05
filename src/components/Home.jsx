4ff3108753337fdf78ecfe5cd26f2439b0520861
elijahthis <emperorelijah007@gmail.com>
2021-12-05 20:31:59 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css/Home.css";
+// import "../css/main.css";
+
+const Home = () => {
+  return (
+    <main>
+      <div className="big-div">
+        <h3 className="my-1">My name is...</h3>
+        <h1 className="my-2">Elijah</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../css/NavBar.css";
+import "../css/main.css";
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Work</li>
+          <li>Contact</li>
+          <li>Interests</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
